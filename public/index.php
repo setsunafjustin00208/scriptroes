7c50f15e56085bd3400cc7a63697f8f5ba24e28e
Tecson, Justin Ed Pierre C. <justin.tecson@gmanmi.com>
2025-05-21 19:21:10 +0800

    build: restore lost files of codeigniter

--- public/index.php
+++ public/index.php
@@ -1,36 +1,7 @@
-#!/usr/bin/env php
 <?php
-
-/**
- * This file is part of CodeIgniter 4 framework.
- *
- * (c) CodeIgniter Foundation <admin@codeigniter.com>
- *
- * For the full copyright and license information, please view
- * the LICENSE file that was distributed with this source code.
- */
 
 use CodeIgniter\Boot;
 use Config\Paths;
-
-/*
- * --------------------------------------------------------------------
- * CODEIGNITER COMMAND-LINE TOOLS
- * --------------------------------------------------------------------
- * The main entry point into the CLI system and allows you to run
- * commands and perform maintenance on your application.
- */
-
-/*
- *---------------------------------------------------------------
- * CHECK SERVER API
- *---------------------------------------------------------------
- */
-
-// Refuse to run when called from php-cgi
-if (str_starts_with(PHP_SAPI, 'cgi')) {
-    exit("The cli tool is not supported when running php-cgi. It needs php-cli to function!\n\n");
-}
 
 /*
  *---------------------------------------------------------------
@@ -38,7 +9,7 @@ if (str_starts_with(PHP_SAPI, 'cgi')) {
  *---------------------------------------------------------------
  */
 
-$minPhpVersion = '8.4'; // If you update this, don't forget to update `public/index.php`.
+$minPhpVersion = '8.4'; // If you update this, don't forget to update `spark`.
 if (version_compare(PHP_VERSION, $minPhpVersion, '<')) {
     $message = sprintf(
         'Your PHP version must be %s or higher to run CodeIgniter. Current version: %s',
@@ -46,12 +17,11 @@ if (version_compare(PHP_VERSION, $minPhpVersion, '<')) {
         PHP_VERSION,
     );
 
-    exit($message);
-}
+    header('HTTP/1.1 503 Service Unavailable.', true, 503);
+    echo $message;
 
-// We want errors to be shown when using it from the CLI.
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+    exit(1);
+}
 
 /*
  *---------------------------------------------------------------
@@ -59,11 +29,13 @@ ini_set('display_errors', '1');
  *---------------------------------------------------------------
  */
 
-// Path to the front controller
-define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR);
+// Path to the front controller (this file)
+define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
 
 // Ensure the current directory is pointing to the front controller's directory
-chdir(FCPATH);
+if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
+    chdir(FCPATH);
+}
 
 /*
  *---------------------------------------------------------------
@@ -84,4 +56,4 @@ $paths = new Paths();
 // LOAD THE FRAMEWORK BOOTSTRAP FILE
 require $paths->systemDirectory . '/Boot.php';
 
-exit(Boot::bootSpark($paths));
+exit(Boot::bootWeb($paths));
